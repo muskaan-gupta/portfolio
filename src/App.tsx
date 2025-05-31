ab3d602d61d62627b58f80685faffde806f00fb3
Muskan Gupta <muskangupta7414@gmail.com>
2025-05-31 14:22:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Moon, Sun } from 'lucide-react';
 import Sidebar from './components/Sidebar';
 import MobileMenu from './components/MobileMenu';
