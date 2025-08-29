94627499288081c0dcbee26503c644711674381a
Muskan Gupta <145460146+muskaan-gupta@users.noreply.github.com>
2025-08-29 14:17:37 +0530

    Update Resume.tsx

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -25,7 +25,7 @@ const Resume: React.FC<ResumeProps> = ({ darkMode }) => {
           </div>
           <div className="flex gap-4">
             <a 
-              href="https://drive.google.com/file/d/1LRYSQ1P39V2SDqdKVS6LPd30LNwSOitg/view?usp=sharing" 
+              href="https://drive.google.com/file/d/1g_gKjarPVZZN4SRdKKwl3yQx8uDxlQqr/view?usp=sharing" 
               target="_blank" 
               rel="noopener noreferrer"
               className={`btn flex items-center gap-2 ${
@@ -36,7 +36,7 @@ const Resume: React.FC<ResumeProps> = ({ darkMode }) => {
               <span>View</span>
             </a>
             <a 
-              href="https://drive.google.com/file/d/1LRYSQ1P39V2SDqdKVS6LPd30LNwSOitg/view?usp=sharing" 
+              href="https://drive.google.com/file/d/1g_gKjarPVZZN4SRdKKwl3yQx8uDxlQqr/view?usp=sharing" 
               download="Muskan's_Resume.pdf"
               className="btn btn-primary flex items-center gap-2"
             >
@@ -56,7 +56,7 @@ const Resume: React.FC<ResumeProps> = ({ darkMode }) => {
         >
           <div className="aspect-[7.5/11] w-full h-full">
             <iframe
-              src="https://drive.google.com/file/d/1LRYSQ1P39V2SDqdKVS6LPd30LNwSOitg/preview"
+              src="https://drive.google.com/file/d/1g_gKjarPVZZN4SRdKKwl3yQx8uDxlQqr/preview"
               title="Resume Preview"
               className="w-full h-full"
               allow="autoplay"
